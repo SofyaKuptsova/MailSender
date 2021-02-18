18a093e54e30485efb6123deefbea56424a8db8a
Sofya <N916-138-9890@yandex.ru>
2021-02-18 17:16:31 +0300

    Тест WPF окно с добавлением полного функционала (отправитель, получатель, данные по серверу, текст письма)

--- Tests/TestWPF/MainWindow.xaml.cs
+++ Tests/TestWPF/MainWindow.xaml.cs
@@ -14,17 +14,22 @@ namespace TestWPF
         {
             InitializeComponent();
         }
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
 
         private void SendButton_Click(object sender, RoutedEventArgs e)
         {
-            var from = new MailAddress("n9161389890@gmail.com", "Софья"); 
-            var to = new MailAddress("N916-138-9890@yandex.ru", "Софья");
+            var from = new MailAddress(EmailFromTb.Text, SenderName.Text); 
+            var to = new MailAddress(EmailToTb.Text, RecipientName.Text);
 
             var message = new MailMessage(from, to);
-            message.Subject = "Заголовок";
-            message.Body = "Текст письма";
+            message.Subject = SubjectText.Text;
+            message.Body = BodyText.Text;
 
-            var client = new SmtpClient("smtp.gmail.com", 25);
+            //var port = Convert.ToInt32(PortTb.Text);
+            var client = new SmtpClient(ServerNameTb.Text, Convert.ToInt32(PortTb.Text));
             client.EnableSsl = true;
 
             client.Credentials = new NetworkCredential
