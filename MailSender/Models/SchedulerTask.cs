065ae9d620d05590a8226770036aeb7798b08c26
Sofya <N916-138-9890@yandex.ru>
2021-02-23 17:14:44 +0300

    Добавление тестовых данных в разметку

---
+++ MailSender/Models/SchedulerTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.Models
+{
+    public class SchedulerTask
+    {
+        public DateTime Time { get; set; }
+        public Server Server { get; set; }
+        public Sender Sender { get; set; }
+        public List<Recipient> Recipients { get; set; }
+        public Message Message { get; set; }
+        //public List<Message> Messages { get; set; } = new List<Message>();
+    }
+}
