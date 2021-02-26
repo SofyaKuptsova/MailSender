f539553011ee559c3f5c024d29b08e2153bfc6cf
Sofya <N916-138-9890@yandex.ru>
2021-02-26 14:22:34 +0300

    Модель-представление Главного окна основного проекта

--- MailSender/MainWindow.xaml.cs
+++ MailSender/MainWindow.xaml.cs
@@ -1,23 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MailSender.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MailSender
 {
     public partial class MainWindow
     {
-        public MainWindow() => InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+            //DataContext = new MainWindowViewModel();
+        }
 
         private void Exit_Click(object sender, RoutedEventArgs e) => Close();
     }
