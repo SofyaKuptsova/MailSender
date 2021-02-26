2b333c1951b8d82ec9d891b827d5ea5e9ed5312e
Sofya <N916-138-9890@yandex.ru>
2021-02-26 13:43:14 +0300

    Модель-представления

--- Tests/TestWPF/MainWindow.xaml.cs
+++ Tests/TestWPF/MainWindow.xaml.cs
@@ -2,55 +2,28 @@
 using System.Net;
 using System.Net.Mail;
 using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
 
 namespace TestWPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+
+    public partial class MainWindow
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-        private void Exit_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
-        }
+        public MainWindow() => InitializeComponent();
 
-        private void SendButton_Click(object sender, RoutedEventArgs e)
-        {
-            var from = new MailAddress(EmailFromTb.Text, SenderName.Text); 
-            var to = new MailAddress(EmailToTb.Text, RecipientName.Text);
-
-            var message = new MailMessage(from, to);
-            message.Subject = SubjectText.Text;
-            message.Body = BodyText.Text;
-
-            var client = new SmtpClient(ServerNameTb.Text, Convert.ToInt32(PortTb.Text));
-            client.EnableSsl = true;
-
-            client.Credentials = new NetworkCredential
-            {
-                UserName = LoginEdit.Text,
-                SecurePassword = PasswordEdit.SecurePassword
-            };
-
-            try
-            {
-                client.Send(message);
-
-                MessageBox.Show("Почта успешно отправлена!", "Отправка почты", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
-            catch (SmtpException)
-            {
-                MessageBox.Show("Ошибка авторизации", "Ошибка отправки почты", MessageBoxButton.OK, MessageBoxImage.Warning);
-            }
-            catch (TimeoutException)
-            {
-                MessageBox.Show("Ошибка адреса сервера", "Ошибка отправки почты", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
     }
+
 }
+
---
+++ Tests/TestWPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using MailSender.lib.ViewModels.Base;
+
+namespace TestWPF.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Test111";
+
+        public string Title 
+        { 
+            get => _Title; 
+            set
+            {
+                _Title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+    }
+}
