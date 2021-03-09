f16c7720df451b6073254718826a65f4bd4acf7e
Sofya <N916-138-9890@yandex.ru>
2021-03-09 18:16:52 +0300

    Пул потоков

--- Tests/TestConsole/ThreadPoolTest.cs
+++ Tests/TestConsole/ThreadPoolTest.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace TestConsole
 {
@@ -10,7 +8,29 @@ namespace TestConsole
     {
         public static void Start()
         {
+            var messages = Enumerable.Range(1, 100).Select(i => $"Message-{i}").ToArray();
 
+            //for (var i = 0; i < messages.Length; i++)
+            //{
+            //    var i0 = i;
+            //    new Thread(() => PrintMessage(messages[i0])).Start();
+            //}
+
+            //ThreadPool.SetMinThreads(15,15);
+            //ThreadPool.SetMaxThreads(20,20);
+            for (var i = 0; i < messages.Length; i++)
+            {
+                var i0 = i;
+                ThreadPool.QueueUserWorkItem(_ => PrintMessage(messages[i0]));
+            }
+        }
+
+        private static void PrintMessage(string Message)
+        {
+            Thread.Sleep(100);
+            Console.WriteLine("id:{0} - {1}",
+                Thread.CurrentThread.ManagedThreadId,
+                Message);
         }
     }
 }
